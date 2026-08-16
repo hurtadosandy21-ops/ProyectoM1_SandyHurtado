705b1f026e13f6d2004f141ea5ae030f726a9260
sanm <hurtadosandy21@gmail.com>
2026-08-15 23:30:58 -0500

    Función para generar códigos de colores hex

--- Desarrollo/app.js
+++ Desarrollo/app.js
@@ -1,2 +1,17 @@
-// seleccion de elementos 
-cosnt btnGenerar = document.getElento
+// Elementos
+
+const btnGenerar = document.getElementById('btn-generar');
+const contenedorPaleta= document.getElementById('contenedor-paleta');
+const selectTamano =document.getElementById('tamano-paleta');
+
+// Función para crear un código de color aleatorio
+
+function generarColor () {
+    const caracteres = '0123456789ABCDEF';
+    let color= '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += caracteres[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
