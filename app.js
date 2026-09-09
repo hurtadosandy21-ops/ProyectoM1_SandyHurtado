5ed23b45d343c61d550d89112c0648b7cf5f95a4
sanm <hurtadosandy21@gmail.com>
2026-09-09 16:58:09 -0500

    Base de app con CSS y JS

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-const btngenerar= document.getElementById(`btn-generar`);
-const btnformato= document.getElementById(`btn-formato`);
-const Cantidad= document.getElementById(`cantidad`);
-const Formato= document.getElementById(`formato`);
+const btnGenerar = document.getElementById(`btn-Generar`);
+const btnFormato = document.getElementById(`btn-Formato`);
+const selectCantidad = document.getElementById(`select-Cantidad`);
+const selectFormato = document.getElementById(`select-Formato`);
+const contenedor = document.getElementById(`contenedor`);
 
-function getRamdonHsl(){
-    const h= Math.floor(Math.ramdon()*360);
-    const s= Math.floor(Math.ramdon()*100);
-    const l= Math.floor(Math.ramdon()*100);
+function getRandomHex(){
+    const letters =`0123456789ABCDEF`;
+    let color= `#`;
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random()* 16)];
+    } 
+    return color; 
 }
-function generarPaletta(){
-    paletteContainer.innerHTML=``;
+function getRandomHSL(){
+    const h = Math.floor(Math.random()*360);
+    const s = Math.floor(Math.random()*100);
+    const l = Math.floor(Math.random()*100);
+    return `hsl(${h}, ${s}% , ${l}%)`;
+}
 
-    const cantidad =parseInt(Cantidad.value,10);
-    const formato = Formato.value;
+function generarPaleta(){
+    if (!contenedor) return;
+    contenedor.innerHTML=``;
 
-    for (let i = 0; i< formato;i++){
-        const colorValue = cantidad ===`HSL`? getRamdonHsl()
+    const cantidad = parseInt (selectCantidad.value, 10);
+    const formato = selectFormato.value;
 
+    for (let i= 0; i <cantidad; i++){
+        const colorValue= formato ===`HEX`? getRandomHex (): getRandomHSL();
+
+        const colorCard =document.createElement(`div`);
+        colorCard.classList.add(`color-card`);
+        colorCard.style.backgroundColor = colorValue;
+
+        const colorText= document.createElement(`span`);
+        colorText.textContent = colorValue;
+
+        colorCard.appendChild(colorText);
+        contenedor.appendChild(colorCard);
     }
 }
+ 
+btnGenerar.addEventListener(`click`,generarPaleta);
+btnFormato.addEventListener(`click`,generarPaleta);
+selectCantidad.addEventListener(`change`,generarPaleta);
+selectFormato.addEventListener(`change`,generarPaleta);
+
+generarPaleta();
